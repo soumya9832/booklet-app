15aa22ebe39cae0ad5fa011486acf4759496d2fa
Soumya <dassoumyadip7407@gmail.com>
2022-09-03 22:00:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/booklet/SplashScrActivity.kt
@@ -0,0 +1,11 @@
+package com.example.booklet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScrActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
